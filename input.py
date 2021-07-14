bcc08de1d738a5eb2176657b4fa9d27b52c41d63
mammad121 <mohammadraoufi84@gmail.com>
2021-07-14 13:21:30 +0430

    input on python

---
+++ input.py
@@ -0,0 +1,13 @@
+print('please enter your name ? >')
+# no
+# output:
+# please enter your name ? >
+# no
+name = input('please enter your name ? >')
+print('{}'.format(name))
+# yes
+# The format that I told you about before
+# output :
+# please enter your name ? >
+# please enter your name ? >mohammad
+# mohammad
