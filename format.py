87eeb2597f149c4c99aa585788b6d7abd6f21370
mammad121 <mohammadraoufi84@gmail.com>
2021-07-14 13:08:38 +0430

    format on python

---
+++ format.py
@@ -0,0 +1,11 @@
+# First we have to ask the name or anything else with the input
+name = input('please enter your name ? >')
+
+# And then with. We put the format of his name in the text
+print('{}'.format(name))
+print(' hi, {} your welcome'.format(name))
+# And with this sign{} we put it in a good place
+# output :
+# please enter your name ? >mohammad
+# mohammad
+#  hi, mohammad your welcome
