0296e89b85660af66c810cf7dfeac40443a41ebe
mammad121 <mohammadraoufi84@gmail.com>
2021-07-15 11:33:51 +0430

    int(input() on python

---
+++ int(input().py
@@ -0,0 +1,20 @@
+name = input('please enter  your name ? >')
+print(' Hi , {}'.format(name))
+# output :
+# please enter  your name ? >mohammad
+#  Hi , mohammad
+age = int(input('please enter your ages ? >'))
+# input() is for a word or sentence, but  int(input() is for a number
+print(' your age {}'.format(age))
+# output :
+# please enter  your name ? >mohammad
+#  Hi , mohammad
+# please enter your ages ? >14
+#  your age 14
+print('your name {} and your age {}'.format(name , age))
+# output :
+# please enter  your name ? >mohammad
+#  Hi , mohammad
+# please enter your ages ? >14
+#  your age 14
+# your name mohammad and your age 14
