3b4b62f1a86728686278a7de37837d551e8d4cc6
mammad121 <mohammadraoufi84@gmail.com>
2021-07-16 13:39:51 +0430

    rage on python

---
+++ range.py
@@ -0,0 +1,58 @@
+print('welcom')
+
+name = input('please enter  your name ? >')
+print(' Hi , {}'.format(name))
+
+number = int(input('please enter your number ? >'))
+
+print(' your number {}'.format(number))
+# We do as before
+print('{} your number :{}'.format(name , number))
+
+for x in range(0 ,number+1):
+   
+    print('number : {}'.format(x))
+
+print('good bye')
+
+# output :
+# welcom
+# please enter  your name ? >mohammad
+#  Hi , mohammad
+# please enter your number ? >12
+#  your number 12
+# mohammad your number :12
+# number : 0
+# number : 1
+# number : 2
+# number : 3
+# number : 4
+# number : 5
+# number : 6
+# number : 7
+# number : 8
+# number : 9
+# number : 10
+# number : 11
+# number : 12
+
+# welcom
+# please enter  your name ? >mohammad
+#  Hi , mohammad
+# please enter your number ? >12
+#  your number 12
+# mohammad your number :12
+# number : 0
+# number : 1
+# number : 2
+# number : 3
+# number : 4
+# number : 5
+# number : 6
+# number : 7
+# number : 8
+# number : 9
+# number : 10
+# number : 11
+# number : 12
+# good bye
